edaf28036ecba64b2dae13e8de19e36d6ae10a8e
yiningtang <yiningchery@gmail.com>
2021-12-11 21:15:35 +1100

    update accordion items

--- src/accordion/components/Accordion.tsx
+++ src/accordion/components/Accordion.tsx
@@ -1,12 +1,10 @@
 import React, { ReactElement } from 'react';
-import { AccordionItems } from '../models/AccordionItems.enum';
 import AccordionItem from './AccordionItem';
 
 export const Accordion = ({ children }: { children: JSX.Element[] }): ReactElement => {
-  const items = children.filter((item: JSX.Element) => item.type && item.type.name === AccordionItems.AccordionItem);
   return (
     <>
-      {items.map((item, index) => {
+      {children.map((item, index) => {
         return (
           <AccordionItem
             key={item.type.name + index}
